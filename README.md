03e930c141ae939a1397aa2d8ef4f0135750ccc8
Josh Philpott <jphil529@gmail.com>
2014-04-14 19:53:43 -0400

    more code cleanup + README

--- README.md
+++ README.md
@@ -3,11 +3,13 @@ micromouse
 
 MicroMouse code for Control of Mobile Robots at USF
 
+Our task was to write code for a robot with three proximity sensors and two stepper motors running on an HC9S12 microcontroller.
+
 Our goal for the implementation:
-  1) Explore the maze with a goal-seeking algorithm
-  2) After finding the goal square in explore, visit all unvisited cells (unless deemed unnecessary)
-  3) Return to (0,0)
-  4) Drive to (4,4) using the shortest path.
+  1. Explore the maze with a goal-seeking algorithm
+  2. After finding the goal square in explore, visit all unvisited cells (unless deemed unnecessary)
+  3. Return to (0,0)
+  4. Drive to (4,4) using the shortest path.
 
 
 
--- explore_test.c
+++ explore_test.c
@@ -1,3 +1,12 @@
+/************************************
+* explore_test.c
+*
+* Joshua Philpott & Michael Goltz
+*
+* explore_test.c is designed to test the drive logic
+* for micromouse. It will also provide a print out of the
+* correct drive sequence
+* ************************************/
 #ifndef FLOODFILL_H
 #define FLOODFILL_H
 #include "floodfill.h"
@@ -5,62 +14,15 @@
 #include <stdio.h>
 
 int walls_real[7][7] = //0-no walls, 1-north wall, 2-east wall 3- north and east wall
-	/*{{2,1,1,1,1,0,0},
-     {1,1,1,0,3,0,0},
-	 {1,1,1,3,0,0,0},
-	 {0,0,0,0,0,0,0},
-	 {0,0,0,0,0,0,0},
-	 {0,0,0,0,0,0,0},
-	 {0,0,0,0,0,0,0}};*/
-	/*{{2,0,2,2,0,0,3},
-     {2,3,2,2,3,0,2},
-	 {2,2,0,1,3,1,2},
-	 {1,0,2,0,1,0,2},
-	 {0,2,0,2,1,2,3},
-	 {1,2,1,3,0,0,2},
-	 {1,1,3,1,1,1,3}};
-	{{3,1,1,3,1,1,3},
-     {0,1,1,2,2,2,2},
-	 {2,3,1,3,2,2,2},
-	 {2,0,3,0,1,2,2},
-	 {0,3,2,2,2,1,2},
-	 {2,2,2,0,1,1,3},
-	 {2,0,0,1,1,1,2}};*/
-
-	/*{{2,0,0,1,1,1,2},
-	 {2,2,2,0,1,1,3},
-	 {0,3,2,2,2,1,2},
-	 {2,0,3,0,1,2,2},
-	 {2,3,1,3,2,2,2},
-	 {0,1,1,2,2,2,2},
-	 {3,1,1,3,1,1,3}};*/
-
-	/*{{2,2,2,2,1,0,2},
-	 {0,1,0,0,1,0,2},
-	 {2,2,2,1,2,3,3},
-	 {0,1,3,0,1,3,2},
-	 {0,2,0,2,1,1,2},
-	 {1,2,1,2,2,1,2},
-	 {1,1,1,1,1,1,3}};*/
-
-	 
-/*	 
-   {{2,1,2,0,0,0,3},
-	{0,0,2,2,3,0,3},
-	{2,3,0,3,2,1,2},
-	{2,0,0,0,0,0,2},
-	{2,3,3,1,3,1,3},
-	{2,0,2,2,0,0,2},
-	{1,1,1,1,1,3,3}};*/
-
-  /* {{2,1,2,1,0,0,2},
-	{0,1,0,0,1,2,3},
-	{2,0,2,1,3,1,2},
-	{2,0,2,0,0,2,2},
-	{2,0,2,2,0,0,3},
-	{2,3,1,3,3,2,2},
-	{1,1,1,1,3,1,3}};*/
-
+	/*
+	Input the maze to test in the following manner -
+	  (0,0)......(0,6)
+		.		   .
+		.          .
+		.		   .
+		.          .
+		.          .
+	  (6,0)......(6,6)*/
    {{2,0,1,1,1,3,2},
 	{0,0,1,3,0,0,2},
 	{2,1,0,2,0,2,2},
@@ -71,11 +33,17 @@ int walls_real[7][7] = //0-no walls, 1-north wall, 2-east wall 3- north and east
 
 
 void addToWalls( int x, int y, int orient, int * front, int * left, int * right){
+/*************************************************
+* addToWalls-populates wall array based off of bots position and orientation.
+*
+* simulates adding walls from the bots perspective.
+* *************************************************/
 	const int WALL = 100;
 	*left = 0;
 	*right = 0;
 	*front = 0;
 
+	//get cells in all directions from the current cell
 	int cell = walls_real[y][x];
 	int cell_west=0;
 	int cell_east=0;
@@ -89,6 +57,7 @@ void addToWalls( int x, int y, int orient, int * front, int * left, int * right)
 	if(y>0)cell_south = walls_real[y-1][x];
 
 	switch(orient){
+		//simulate wall appearing based off orientation
 		case 0:
 			if(cell_west==2 || cell_west==3)*left=WALL;
 			if(cell==1 || cell==3)*front=WALL;
@@ -135,29 +104,23 @@ void addToWalls( int x, int y, int orient, int * front, int * left, int * right)
 int main(){
 	int x=0;
 	int y=0;
-
 	int orient=0; //north orientation.
-
 	int left=0;
 	int right=0;
 	int front=0;
+	int flag = 0;
 
-	int dir_next=0;
-
-	int flag= 0;
-
-	initMaze();
-
+	initMaze(); //set walls[][] to 0
 
 	while(1){
 		flag++;
-		if(flag>1000)break;
+		if(flag>1000)break; //MAX 1000 moves.
 		addToWalls(x,y,orient,&front, &left, &right);
 		orient=explore(x,y,4,4,orient,front,left,right);
-		//printMaze();
-		//printMaze();
-		/*printFMaze();
-		printVMaze();*/
+		//uncomment to print walls array, flood array (fnum), and visited array
+	    //printMaze();
+		//printFMaze();
+		//printVMaze();
 		if(orient==4){
 			printf("finished in %d moves.\n", flag);
 			break;
@@ -165,6 +128,7 @@ int main(){
 		if(orient==-1){
 			printf("ERROR\n");break;
 		}
+		//update position
 		switch(orient){
 			case 0:y++;break;
 			case 1:x++;break;
